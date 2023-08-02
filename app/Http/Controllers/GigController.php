0a2f016fb3943cb7f416d577d898ff3badeb2874
Jacobson <romeojacobson@gmail.com>
2023-08-02 09:46:58 +0300

    Added date field

--- app/Http/Controllers/GigController.php
+++ app/Http/Controllers/GigController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Gig;
 use Illuminate\Http\Request;
+use phpseclib3\Crypt\RC2;
 
 class GigController extends Controller
 {
@@ -14,20 +15,31 @@ class GigController extends Controller
             'description' => $request->description,
             'location' => $request->location,
             'price' => $request->price,
+            'date'=> $request->date
         ]);
 
         return response()->json([
-            'message'=> $gig->title .' gig created successfully'
+            'message'=> $gig->title .' gig created successfully',
+            'gig'=>$gig
         ],200);
     }
 
-    public function listGigs(Request $request)
+    public function listGigs()
     {
         $gigs = Gig::all();
 
         return response()->json([
             'gigs' => $gigs
         ],200);
+    }
+
+    public function getGig($id)
+    {
+        $gig = Gig::find($id);
+
+        return response()->json([
+            'gig' => $gig
+        ], 200);
     }
 
    
--- app/Models/Gig.php
+++ app/Models/Gig.php
@@ -14,6 +14,7 @@ class Gig extends Model
         'description',
         'price',
         'location',
+        'date'
     ];
 
 }
