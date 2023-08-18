2f63f0f21688ea83e48737f8c9867be2dc4c1b79
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-18 22:06:50 +0300

    Update GigController.php
    Added edit and delete actions in the controller

--- app/Http/Controllers/GigController.php
+++ app/Http/Controllers/GigController.php
@@ -27,6 +27,7 @@ class GigController extends Controller
         ],200);
     }
 
+    // Listing all gigs
     public function listGigs()
     {
         $gigs = Gig::all()->load('user');
@@ -36,6 +37,7 @@ class GigController extends Controller
         ],200);
     }
 
+    // Getting a single gig by ID
     public function getGig($id)
     {
         $gig = Gig::find($id)->load('user');
@@ -43,6 +45,40 @@ class GigController extends Controller
         return response()->json([
             'gig' => $gig
         ], 200);
+    }
+
+    // Deleting a gig
+    public function getGig(Gig $gig)
+    {
+        $gig->delete();
+        $gigs = Gig::all()->load('user');
+
+        return response()->json([
+            'message' => "Gig deleted successfully",
+            'gigs' => $gigs                    
+        ], 200);
+    }
+
+     public function editGig(Request $request)
+    {
+        $gig = Gig::where('id', $request->id)
+                    ->get()
+                    ->update([
+                             'title' => $request->title,
+                            'description' => $request->description,
+                            'location' => $request->location,
+                            'price' => $request->price,
+                            'date'=> $request->date,
+                            'user_id'=> $request->userId          
+                    ]);
+
+        $gigs = Gig::all()->load("user");
+
+        return response()->json([
+            'message'=> 'Gig updated successfully',
+            // 'gig'=>$gig,
+            'gigs'=>$gigs                           
+        ],200);
     }
 
    
